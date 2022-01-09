2893fecc68e0824cb770cd4dc146631514d9b1a0
akuma1341 <akuma1341@gmail.com>
2022-01-09 13:22:27 +0300

    Driver update

--- src/main/java/com/example/springmovies/controllers/MoviesController.java
+++ src/main/java/com/example/springmovies/controllers/MoviesController.java
@@ -1,13 +1,11 @@
 package com.example.springmovies.controllers;
 
-import com.example.springmovies.entities.Actors;
 import com.example.springmovies.entities.Movies;
 import com.example.springmovies.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/movies")
